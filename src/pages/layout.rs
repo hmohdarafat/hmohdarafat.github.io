12e9b280c167b3ea265a31e96834ef34279d7248
Mohd Arafat Hossain <hmohdarafat@gmail.com>
2026-05-30 22:36:25 +0600

    v0.1.1

--- src/pages/layout.rs
+++ src/pages/layout.rs
@@ -3,20 +3,61 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn SiteLayout() -> Element {
+    let current_route = use_route::<Route>();
+
+    let home_active = matches!(&current_route, Route::Home {});
+    let resume_active = matches!(&current_route, Route::Resume {});
+    let portfolio_active = matches!(&current_route, Route::Portfolio {});
+    let blog_active = matches!(&current_route, Route::Blog {});
+
     rsx! {
         div { class: "site-shell",
             nav { class: "navbar",
                 Link { to: Route::Home {}, class: "brand",
-                    span { class: "brand-mark", "AH" }
-                    span { "hmohdarafat.github.io" }
+                    img {
+                        class: "brand-image",
+                        src: asset!("/assets/images/profile_pic_mini.jpg"),
+                        alt: "Mohd Arafat Hossain profile picture"
+                    }
+
+                    span { class: "brand-text",
+                        span { class: "brand-name", "Mohd Arafat Hossain" }
+                        span { class: "brand-bracket", " [ " }
+                        span { class: "brand-domain", "hmohdarafat.github.io" }
+                        span { class: "brand-bracket", " ] " }
+                    }
                 }
 
                 div { class: "nav-links",
-                    Link { to: Route::Home {}, class: "nav-link", "Home" }
-                    Link { to: Route::Resume {}, class: "nav-link", "Resume" }
-                    Link { to: Route::Portfolio {}, class: "nav-link", "Portfolio" }
-                    Link { to: Route::Blog {}, class: "nav-link", "Blog" }
-                    a { class: "nav-link", href: "mailto:hmohdarafat@gmail.com", "Contact" }
+                    Link {
+                        to: Route::Home {},
+                        class: if home_active { "nav-link active" } else { "nav-link" },
+                        "Home"
+                    }
+
+                    Link {
+                        to: Route::Resume {},
+                        class: if resume_active { "nav-link active" } else { "nav-link" },
+                        "Resume"
+                    }
+
+                    Link {
+                        to: Route::Portfolio {},
+                        class: if portfolio_active { "nav-link active" } else { "nav-link" },
+                        "Portfolio"
+                    }
+
+                    Link {
+                        to: Route::Blog {},
+                        class: if blog_active { "nav-link active" } else { "nav-link" },
+                        "Blog"
+                    }
+
+                    a {
+                        class: "nav-link",
+                        href: "mailto:hmohdarafat@gmail.com",
+                        "Contact"
+                    }
                 }
             }
 
